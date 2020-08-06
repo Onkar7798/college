588a8948a120a441226e548ee70ba9d88383f72e
Onkar <2016.onkar.kajrolkar@ves.ac.in>
2020-08-06 19:46:50 +0530

    Search function remaining

--- Colleges.py
+++ Colleges.py
@@ -1,4 +1,23 @@
+import tkinter as tk
+from tkinter import *
 import csv
+from functools import *
+
+root = tk.Tk()
+root.title("Colleges")
+root.geometry("300x300")
+global top
+top = tk.Frame(root)
+top.grid()
+global bottom
+bottom = tk.Frame(root)
+bottom.grid()
+
+
+def hide_bottom():
+    for widget in bottom.winfo_children():
+        widget.destroy()
+
 
 class College:
     def __init__(self, cId, cName, course, city, fees, pin):
@@ -13,39 +32,79 @@ class College:
         with open('colleges.csv', 'a') as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow([self.collegeId, self.collegeName, self.courseType, self.city, self.fees, self.pinCode])
-        print("Successfully Registered College")
+        l1 = Label(bottom, text="Successfully registered college!")
+        l1.grid(columnspan=3, row=8)
 
-def newCollege():
-    print("Enter details of the college")
-    cId = input("College Id: ")
-    cName = input("College name: ")
-    course = input("Course type: ")
-    city = input("City: ")
-    fees = input("Fees: ")
-    pin = input("Pincode: ")
+
+def newCollege(cId, cName, course, city, fees, pin):
     obj = College(cId, cName, course, city, fees, pin)
     obj.register()
 
-def searchCollege():
-    print("Enter the search parameters")
-    cName = input("Enter college name: ")
-    course = input("Enter course type: ")
+
+def register():
+    hide_bottom()
+    l1 = Label(bottom, text="College Id: ")
+    l1.grid(column=0, row=1)
+    cId = Entry(bottom, width=30)
+    cId.grid(column=1, row=1, columnspan=2)
+    l2 = Label(bottom, text="College Name: ")
+    l2.grid(column=0, row=2)
+    cName = Entry(bottom, width=30)
+    cName.grid(column=1, row=2, columnspan=2)
+    l3 = Label(bottom, text="Course: ")
+    l3.grid(column=0, row=3)
+    course = Entry(bottom, width=30)
+    course.grid(column=1, row=3, columnspan=2)
+    l4 = Label(bottom, text="City: ")
+    l4.grid(column=0, row=4)
+    city = Entry(bottom, width=30)
+    city.grid(column=1, row=4, columnspan=2)
+    l5 = Label(bottom, text="Fees: ")
+    l5.grid(column=0, row=5)
+    fees = Entry(bottom, width=30)
+    fees.grid(column=1, row=5, columnspan=2)
+    l6 = Label(bottom, text="Pincode: ")
+    l6.grid(column=0, row=6)
+    pin = Entry(bottom, width=30)
+    pin.grid(column=1, row=6, columnspan=2)
+    btn = Button(bottom, text="Submit",
+                 command=lambda: newCollege(cId.get(), cName.get(), course.get(), city.get(), fees.get(), pin.get()))
+    btn.grid(columnspan=3, row=7)
+
+
+def searchCollege(cName, course):
+    l1 = Label(bottom)
+    l1.grid(columnspan=3, row=4)
     success = True
     with open('colleges.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
         for i in csv_reader:
             if cName in i and course in i:
                 if success:
-                    print("Colleges Found")
+                    l1.configure(text="Colleges Found!")
                     success = False
                 print("College Id: ", i[0], ", College Name: ", i[1], ", Course: ", i[2], ", City: ", i[3], ", Fees: ",
                       i[4], ", Pincode", i[5])
     if success:
-        print("No Colleges found for the given name and course")
+        l1.configure(text="No Colleges found for the given name and course")
 
-def removeCollege():
+
+def search():
+    hide_bottom()
+    l1 = Label(bottom, text="College Name: ")
+    l1.grid(column=0, row=1)
+    cName = Entry(bottom, width=30)
+    cName.grid(column=1, row=1, columnspan=2)
+    l2 = Label(bottom, text="Course: ")
+    l2.grid(column=0, row=2)
+    course = Entry(bottom, width=30)
+    course.grid(column=1, row=2, columnspan=2)
+    btn = Button(bottom, text="Submit", command=lambda: searchCollege(cName.get(), course.get()))
+    btn.grid(columnspan=3, row=3)
+
+
+def removeCollege(cId):
     li = list()
-    cId = input("Enter college Id: ")
     with open('colleges.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
         for i in csv_reader:
@@ -53,26 +112,29 @@ def removeCollege():
     with open('colleges.csv', 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         for lines in li:
-            if lines[0] == cId:
+            if lines[0] == str(cId):
                 continue
             else:
                 csv_writer.writerow(lines)
-    print("College of id: ", cId, " removed successfully")
+    str1 = "College with Id: " + str(cId) + " removed successfully!"
+    l1 = Label(bottom, text=str1)
+    l1.grid(columnspan=3, row=3)
 
-def main():
-    print("Select an option")
-    choice = int(input("1. Register new College\n2. Search a college\n3. Remove college\n4. Exit"))
-    if choice == 1:
-        newCollege()
-    elif choice == 2:
-        searchCollege()
-    elif choice == 3:
-        removeCollege()
-    elif choice == 4:
-        print("Bye")
-        quit()
-    else:
-        print("Invalid Input")
-    main()
 
-main()
+def remove():
+    hide_bottom()
+    l1 = Label(bottom, text="College Id: ")
+    l1.grid(column=0, row=1)
+    cId = Entry(bottom, width=30)
+    cId.grid(column=1, row=1, columnspan=2)
+    btn = Button(bottom, text="Submit", command=lambda: removeCollege(cId.get()))
+    btn.grid(columnspan=3, row=2)
+
+
+bt1 = Button(top, text="Register College", command=register)
+bt1.grid(column=0, row=0)
+bt2 = Button(top, text="Search Colleges", command=search)
+bt2.grid(column=1, row=0)
+bt3 = Button(top, text="Remove College", command=remove)
+bt3.grid(column=2, row=0)
+root.mainloop()
