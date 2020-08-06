98e3af84e1ce13193be1fdd8f9e6a897fa7ef443
Onkar <2016.onkar.kajrolkar@ves.ac.in>
2020-08-06 15:18:03 +0530

    Sample GUI

--- test.py
+++ test.py
@@ -1,7 +1,68 @@
-import csv
-csv_file2 = open('colleges.csv', 'a')
-csv_writer = csv.writer(csv_file2)
-li = ['1','3424','fag','sgs','dgsfgs','sdsdf']
-csv_writer.writerow(li)
+import tkinter as tk
+from tkinter import *
 
-csv_file2.close()
+root = tk.Tk()
+root.title("Colleges")
+global top
+top = tk.Frame(root).grid()
+global bottom
+bottom = tk.Frame(root).grid()
+
+
+def success():
+    li = bottom.grid_slaves()
+    for l in li:
+        l.destroy()
+
+def newCollege():
+    l1 = Label(bottom, text="College Id: ")
+    l1.grid(column=0,row=1)
+    txt1 = Entry(bottom, width=30)
+    txt1.grid(column=1, row=1, columnspan=2)
+    l2 = Label(bottom, text="College Name: ")
+    l2.grid(column=0, row=2)
+    txt2 = Entry(bottom, width=30)
+    txt2.grid(column=1, row=2, columnspan=2)
+    l3 = Label(bottom, text="Course: ")
+    l3.grid(column=0, row=3)
+    txt3 = Entry(bottom, width=30)
+    txt3.grid(column=1, row=3, columnspan=2)
+    l4 = Label(bottom, text="City: ")
+    l4.grid(column=0, row=4)
+    txt4 = Entry(bottom, width=30)
+    txt4.grid(column=1, row=4, columnspan=2)
+    l5 = Label(bottom, text="Fees: ")
+    l5.grid(column=0, row=5)
+    txt5 = Entry(bottom, width=30)
+    txt5.grid(column=1, row=5, columnspan=2)
+    l6 = Label(bottom, text="Pincode: ")
+    l6.grid(column=0, row=6)
+    txt6 = Entry(bottom, width=30)
+    txt6.grid(column=1, row=6, columnspan=2)
+    btn = Button(bottom, text="Submit", command=success)
+    btn.grid(columnspan=3, row=7)
+
+def searchCollege():
+    l1 = Label(bottom, text="College Name: ")
+    l1.grid(column=0, row=1)
+    txt1 = Entry(bottom, width=30)
+    txt1.grid(column=1, row=1, columnspan=2)
+    l2 = Label(bottom, text="Course: ")
+    l2.grid(column=0, row=2)
+    txt2 = Entry(bottom, width=30)
+    txt2.grid(column=1, row=2, columnspan=2)
+    btn = Button(bottom, text="Submit", command=success)
+    btn.grid(columnspan=3, row=3)
+
+def removeCollege():
+    pass
+
+bt1 = Button(top, text = "Register College", command=newCollege)
+bt1.grid(column=0, row=0)
+bt2 = Button(top, text="Search Colleges", command=searchCollege)
+bt2.grid(column=1, row=0)
+bt3 = Button(top, text="Remove College", command=removeCollege)
+bt3.grid(column=2, row=0)
+
+
+root.mainloop()
